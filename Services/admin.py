2316ef88b79acf2e39ebbea8b00a957b10b98b95
ieSpring98 <mohammad_76kiani@yahoo.com>
2019-02-17 18:38:22 +0330

    add admin page

---
+++ Services/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from Services.models import Project, ProjectSkill, Skill
+
+admin.site.register(Project)
+admin.site.register(ProjectSkill)
+admin.site.register(Skill)
